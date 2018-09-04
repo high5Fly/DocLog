418d7b220a943fe954947202fcf2d117fc0d62d2
Miroslav Danazhiev <m.danadjiev@yahoo.com>
2018-09-04 09:40:35 +0300

    Initial commit

---
+++ DocLog/DocLog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DocLog
+//
+//  Created by Miroslav Danazhiev on 4.09.18.
+//  Copyright © 2018 Miroslav Danazhiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
